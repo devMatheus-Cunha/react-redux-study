3f2d9895c247721f6e364bc2d87d07198d39375e
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-28 07:58:24 -0300

    feat: component base Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Card: React.FC = () => {
+  return (
+    <Container>
+      Card
+    </Container>
+  );
+}
+
+export default Card;
