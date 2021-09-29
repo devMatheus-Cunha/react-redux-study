dd157efd86bf3d32cea564d601fe3fe65c204864
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-29 07:25:01 -0300

    feat: component Media

---
+++ src/components/Media/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../Card";
+
+const Media: React.FC = () => {
+  return (
+      <Card colors="red" title="Media dos numeros">
+        <span>
+          <span>Resultado:</span>
+          <strong>5</strong>
+        </span>
+      </Card>
+  );
+};
+
+export default Media;
