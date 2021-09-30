60b81073f33be0e19f78ae8465d55fca9cf2aec3
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-30 07:55:25 -0300

    feat: logic without redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 import Interval from "./components/Interval";
@@ -10,14 +10,24 @@ import Sum from "./components/Sum";
 import { GlobalStyle, Flex } from "./assets/global";
 
 function App() {
+  // states
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(100);
+
   return (
     <>
       <h5>Exercpicio React-Redux</h5>
-      <Interval />
+      <Interval min={min} max={max} onChangeMin={setMin} onChangeMax={setMax}/>
       <Flex>
-        <Media>Ola familia</Media>
-        <Sum>Ola familia</Sum>
-        <Draw>Ola familia</Draw>
+        <Media min={min} max={max} >
+          Ola familia
+        </Media>
+        <Sum min={min} max={max}>
+          Ola familia
+        </Sum>
+        <Draw min={min} max={max}>
+          Ola familia
+        </Draw>
       </Flex>
       <GlobalStyle />
     </>
