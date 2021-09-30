6ce2c591c00ac2e4387a47d625ac3f8ce5676118
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-30 07:54:41 -0300

    feat: getting new logic

--- src/components/Interval/index.tsx
+++ src/components/Interval/index.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import Card from "../Card";
 
 import { Container } from "./styles";
+import { IIntervalProps } from "../shared/interface";
 
-const Interval: React.FC = () => {
+const Interval = ({ min, max, onChangeMin, onChangeMax }: IIntervalProps) => {
   return (
     <Container>
       <Card colors="gray" title="Intervalo de numeros">
         <span>
           <strong>Mínimo:</strong>
-          <input type="number" value={0} readOnly />
+          <input
+            type="number"
+            value={min}
+            onChange={(event) => onChangeMin(parseFloat(event.target.value))}
+          />
         </span>
         <span>
           <strong>Máximo:</strong>
-          <input type="number" value={10} readOnly />
+          <input
+            type="number"
+            value={max}
+            onChange={(event) => onChangeMax(parseFloat(event.target.value))}
+          />
         </span>
       </Card>
     </Container>
