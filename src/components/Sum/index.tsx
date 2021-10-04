81648694754571fb78a9c661b865bcfa81557ade
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-04 07:46:36 -0300

    feat: connect redux with page Sum

--- src/components/Sum/index.tsx
+++ src/components/Sum/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 
 //components
 import Card from "../Card";
 
-const Sum = () => {
+const Sum = ({ min, max }: any) => {
   return (
     <Card colors="blue" title="Soma dos numeros">
       <span>
         <span>Resultado:</span>
-        <strong>9</strong>
+        <strong>{min + max}</strong>
       </span>
     </Card>
   );
 };
 
-export default Sum;
+const mapStateToPros = (state: any) => {
+  return {
+    min: state.numbers.min,
+    max: state.numbers.max,
+  };
+};
+
+export default connect(mapStateToPros)(Sum);
