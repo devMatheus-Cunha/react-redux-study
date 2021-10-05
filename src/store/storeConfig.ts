728304fc39c10604f9ea27b529c9aec3f2d2b06a
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-05 12:51:42 -0300

    feat: config chage value of the new state

--- src/store/storeConfig.ts
+++ src/store/storeConfig.ts
@@ -1,14 +1,20 @@
 import { createStore, combineReducers } from "redux";
 
 const reducers = combineReducers({
-  numbers: function (state, action) {
-    return {
-      min: 10,
-      max: 30,
-    };
-  },
-  names: function (state, action) {
-    return ["ana", "bia", "lu"];
+  numbers: function (state: any, action: any) {
+    console.log(action)
+    switch (action.type) {
+      case "CHANGE_MIN_NUMBER":
+        return {
+          ...state,
+          min: action.payload,
+        };
+      default:
+        return {
+          min: 1,
+          max: 30,
+        };
+    }
   },
 });
 
@@ -17,3 +23,4 @@ function storeConfig() {
 }
 
 export default storeConfig;
+
